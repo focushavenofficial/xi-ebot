b469e1d5a38294084305e509bbdf2af5427ecd1f
NIGHT-GAMES-20 <night20games@gmail.com>
2025-05-04 10:05:14 +0000

    pm2 fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,14 +1,11 @@
-export default {
-    apps: [{
-      name: 'afk-bot',
-      script: './bot.js',  // replace with your entry point
-      watch: true,
-      autorestart: true,
-      max_restarts: 10,
-      restart_delay: 5000,
-      env: {
-        NODE_ENV: 'production',
-      },
-    }]
-  };
-  
+export const apps = [{
+  name: 'afk-bot',
+  script: './bot.js',
+  watch: true,
+  autorestart: true,
+  max_restarts: 10,
+  restart_delay: 5000,
+  env: {
+    NODE_ENV: 'production',
+  },
+}];
